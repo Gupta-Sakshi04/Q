7285f0af974def68020528f0ff91a41569eeb9d2
Gupta-Sakshi04 <83657212+Gupta-Sakshi04@users.noreply.github.com>
2021-08-26 00:48:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken')
 const jwt_secret = require('./auth/jwtauth')
 require('dotenv').config()
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/qzeal', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/qzeal', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useCreateIndex: true
